df17b3a08c54dde84d293b6d03540d205fd4422a
Shijo T <34747526+AngelBond14@users.noreply.github.com>
2020-03-03 13:49:35 -0500

    Update arrayProduct.java

--- arrayProduct.java
+++ arrayProduct.java
@@ -1,1 +1,20 @@
+//Steven Pierre, Avery Chiappetta-Aponte, Shijo Thomas
+// Chapter 8 Group Lab, Group 2
+public class arrayProduct
+{
+	public static void main(String args[])
+	{
+		int [] array = {5,10,20,50};
+		System.out.println("The product of the array is " + arrayProduct.getProduct(array) );
+	}
 
+	public static int getProduct(int [] a)
+	{
+		int total = a[0];
+		for (int i = 1; i < a.length; i++)
+		{
+			total = total * a[i];
+		}
+		return total;
+	}
+}
